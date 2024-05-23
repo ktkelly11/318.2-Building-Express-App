a04fe9068cafd6fa623266ffcf43226a1d731cfd
Katherine Kelly <ktkelly11@gmail.com>
2024-05-22 22:55:26 -0400

    created views

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const fs = require("fs");
+
+// serve static files from the styles directory
+app.use(express.static("./styles"));
+
+// Part 1: Routes, Templates, and Views
 
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
